7ca4da55f6fba8c8968ebd251ea1505098b5b5e3
Charles Ludwig <charleswludwig@gmail.com>
2023-11-12 17:52:33 -0500

    covid vaccine schedule

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 from pprint import pprint
 import csv
 from dotenv import load_dotenv
-import sqlalchemy
 import pandas as pd
 
 load_dotenv()
@@ -24,9 +23,10 @@ if version:
 else:
     print('Not connected.')
 
-table_1 = os.getenv('TABLE1')
-table_2 = os.getenv('TABLE2')
-table_3 = os.getenv('TABLE3')
+# To protect against SQL Injection attacks, parameterizing my queries
+table_1 = os.getenv('TABLE1') # City
+table_2 = os.getenv('TABLE2') # Country
+table_3 = os.getenv('TABLE3') # CountryLanguage
 
 try:
     query = f"Select * from {table_1};"
@@ -84,14 +84,14 @@ sweden_group = by_country.get_group("Sweden")
 usa_group = by_country.get_group("United States")
 
 # Saving different groups to different CSV files (for posterity)
-brazil_group.to_csv('datasets/groups/brazil_group.csv')
-china_group.to_csv('datasets/groups/china_group.csv')
-egypt_group.to_csv('datasets/groups/egypt_group.csv')
-sweden_group.to_csv('datasets/groups/sweden_group.csv')
-usa_group.to_csv('datasets/groups/usa_group.csv')
+brazil_group.to_csv('brazil_group.csv')
+china_group.to_csv('china_group.csv')
+egypt_group.to_csv('egypt_group.csv')
+sweden_group.to_csv('sweden_group.csv')
+usa_group.to_csv('usa_group.csv')
        
 # Extracting countries by their populations and number of cities
-merged_dataframe.groupby("CountryName")["CountryPopulation"].value_counts().to_csv('datasets/country_by_population_and_cities.csv')
+merged_dataframe.groupby("CountryName")["CountryPopulation"].value_counts().to_csv('country_by_population_and_cities.csv')
 
 country_population = merged_dataframe.groupby("CountryName")["CountryPopulation"].value_counts()
 
@@ -103,85 +103,105 @@ egypt_dict = {}
 sweden_dict = {}
 usa_dict = {}
 
+# Extracting individual country's population weights in the respective vaccine allocation regions
 for key, value in country_population_dict.items():
     # Separating keys in tuple variable 'key'
     key_1, key_2 = key
 
     # For Countries bordering Brazil
     if key_1 == "Argentina":
-        brazil_dict[key_1] = round(150000 / (key_2 / value), 2)    
+        brazil_dict[key_1] = key_2 
 
     if key_1 == "Bolivia":
-        brazil_dict[key_1] = round(150000 / (key_2 / value), 2)    
+        brazil_dict[key_1] = key_2 
+        
     if key_1 == "Colombia":
-        brazil_dict[key_1] = round(150000 / (key_2 / value), 2)    
+        brazil_dict[key_1] = key_2 
+        
     if key_1 == "French Guiana":
-        brazil_dict[key_1] = round(150000 / (key_2 / value), 2)    
+        brazil_dict[key_1] = key_2 
+          
     if key_1 == "Guyana":
-        brazil_dict[key_1] = round(150000 / (key_2 / value), 2)    
+        brazil_dict[key_1] = key_2 
+
     if key_1 == "Paraguay":
-        brazil_dict[key_1] = round(150000 / (key_2 / value), 2)    
+        brazil_dict[key_1] = key_2 
+
     if key_1 == "Peru":
-        brazil_dict[key_1] = round(150000 / (key_2 / value), 2)    
+        brazil_dict[key_1] = key_2 
+
     if key_1 == "Suriname":
-        brazil_dict[key_1] = round(150000 / (key_2 / value), 2)    
+        brazil_dict[key_1] = key_2 
+
     if key_1 == "Uruguay":
-        brazil_dict[key_1] = round(150000 / (key_2 / value), 2)    
+        brazil_dict[key_1] = key_2
+
     if key_1 == "Venezuela":
-        brazil_dict[key_1] = round(150000 / (key_2 / value), 2)    
+        brazil_dict[key_1] = key_2  
 
     # For Countries bordering China
     if key_1 == "Afghanistan":
-        china_dict[key_1] = round(200000 / (key_2 / value), 2)    
+        china_dict[key_1] = key_2 
+
     if key_1 == "Bhutan":
-        china_dict[key_1] = round(200000 / (key_2 / value), 2)    
+        china_dict[key_1] = key_2 
+
     if key_1 == "India":
-        china_dict[key_1] = round(200000 / (key_2 / value), 2)    
+        china_dict[key_1] = key_2  
     if key_1 == "Kazakstan":
-        china_dict[key_1] = round(200000 / (key_2 / value), 2)    
+        china_dict[key_1] = key_2  
     if key_1 == "North Korea":
-        china_dict[key_1] = round(200000 / (key_2 / value), 2)    
+        china_dict[key_1] = key_2  
     if key_1 == "Kyrgyzstan":
-        china_dict[key_1] = round(200000 / (key_2 / value), 2)    
+        china_dict[key_1] = key_2 
     if key_1 == "Laos":
-        china_dict[key_1] = round(200000 / (key_2 / value), 2)    
+        china_dict[key_1] = key_2 
     if key_1 == "Mongolia":
-        china_dict[key_1] = round(200000 / (key_2 / value), 2)    
+        china_dict[key_1] = key_2 
     if key_1 == "Myanmar":
-        china_dict[key_1] = round(200000 / (key_2 / value), 2)    
+        china_dict[key_1] = key_2 
+
     if key_1 == "Burma":
-        china_dict[key_1] = round(200000 / (key_2 / value), 2)    
+        china_dict[key_1] = key_2
+
     if key_1 == "Pakistan":
-        china_dict[key_1] = round(200000 / (key_2 / value), 2)    
+        china_dict[key_1] = key_2 
+
     if key_1 == "Russia":
-        china_dict[key_1] = round(200000 / (key_2 / value), 2)    
+        china_dict[key_1] = key_2 
+
     if key_1 == "Mongolia":
-        china_dict[key_1] = round(200000 / (key_2 / value), 2)    
+        china_dict[key_1] = key_2 
+
     if key_1 == "Tajikistan":
-        china_dict[key_1] = round(200000 / (key_2 / value), 2)    
+        china_dict[key_1] = key_2 
+
     if key_1 == "Vietnam":
-        china_dict[key_1] = round(200000 / (key_2 / value), 2)    
+        china_dict[key_1] = key_2
+
     # For Countries bordering Egypt
     if key_1 == "Israel":
-        egypt_dict[key_1] = round(50000 / (key_2 / value), 2)    
+        egypt_dict[key_1] = key_2 
+
     if key_1 == "Libya":
-        egypt_dict[key_1] = round(50000 / (key_2 / value), 2)    
+        egypt_dict[key_1] = key_2 
+
     if key_1 == "Sudan":
-        egypt_dict[key_1] = round(50000 / (key_2 / value), 2)    
+        egypt_dict[key_1] = key_2 
 
     # For Countries bordering Sweden
     if key_1 == "Finland":
-        sweden_dict[key_1] = round(50000 / (key_2 / value), 2)    
+        sweden_dict[key_1] = key_2 
 
     if key_1 == "Norway":
-        sweden_dict[key_1] = round(50000 / (key_2 / value), 2)    
+        sweden_dict[key_1] = key_2  
 
     # For Countries bordering USA
     if key_1 == "Canada":
-        usa_dict[key_1] = round(150000 / (key_2 / value), 2)    
-    
+        usa_dict[key_1] = key_2
+
     if key_1 == "Mexico":
-        usa_dict[key_1] = round(150000 / (key_2 / value), 2)    
+        usa_dict[key_1] = key_2  
 
 pprint(brazil_dict)
 pprint(china_dict)
@@ -189,6 +209,176 @@ pprint(egypt_dict)
 pprint(sweden_dict)
 pprint(usa_dict)
 
+# This is a cluster of how many people per vaccine in the group of countries
+# print(population_per_vaccine)
+
+vaccine_allotment_dict = {
+    "Brazil": 150000,
+    "China": 200000,
+    "Egypt": 50000, 
+    "Sweden": 50000,
+    "United States": 150000
+}
+
+total_vaccines_alloted = 0
+
+for v_key, v_value in vaccine_allotment_dict.items():    
+    if v_key == "Brazil":
+        new_dict = {
+            'Population': 0
+        } 
+
+        total_population_counter = 0
+        counter_index = 0
+
+        for key, value in brazil_dict.items():
+#           print(f"{key} has {value} people")
+            total_population_counter += value
+            counter_index += 1
+            new_dict['Population'] += value
+
+        for key, value in new_dict.items():
+            new_dict['Population'] = value
+
+#        print(f"There are {total_population_counter} people in {counter_index} countries.")
+
+        mean = total_population_counter / counter_index
+
+        population_per_vaccine = total_population_counter / mean
+
+        for key, value in brazil_dict.items():
+            sum = v_value / population_per_vaccine
+            print(f"{key}'s Vaccine allotment:  {sum}")
+            print(f"\tPer city center, {key} will have {round(total_population_counter / value, 2)} people per vaccine")
+            total_vaccines_alloted = sum * counter_index
+
+        print(f"Total vaccines alloted for the {counter_index} countries in the {v_key} Group: {int(total_vaccines_alloted)}\n")
+
+    if v_key == "China":
+        new_dict = {
+            'Population': 0
+        } 
+
+        total_population_counter = 0
+        counter_index = 0
+
+        for key, value in china_dict.items():
+#           print(f"{key} has {value} people")
+            total_population_counter += value
+            counter_index += 1
+            new_dict['Population'] += value
+
+        for key, value in new_dict.items():
+            new_dict['Population'] = value
+
+#       print(f"There are {total_population_counter} people in {counter_index} countries.")
+
+        mean = total_population_counter / counter_index
+
+        population_per_vaccine = total_population_counter / mean
+
+        for key, value in china_dict.items():
+            sum = v_value / population_per_vaccine
+            print(f"{key}'s Vaccine allotment:  {int(sum)}")
+            print(f"\tPer city center, {key} will have {round(total_population_counter / value, 2)} people per vaccine")
+            total_vaccines_alloted = sum * counter_index
+
+        print(f"Total vaccines alloted for the {counter_index} countries in the {v_key} Group: {int(total_vaccines_alloted)}\n")
+
+    if v_key == "Egypt":
+        new_dict = {
+            'Population': 0
+        } 
+
+        total_population_counter = 0
+        counter_index = 0
+
+        for key, value in egypt_dict.items():
+#           print(f"{key} has {value} people")
+            total_population_counter += value
+            counter_index += 1
+            new_dict['Population'] += value
+
+        for key, value in new_dict.items():
+            new_dict['Population'] = value
+
+#       print(f"There are {total_population_counter} people in {counter_index} countries.")
+
+        mean = total_population_counter / counter_index
+
+        population_per_vaccine = total_population_counter / mean
+
+        for key, value in egypt_dict.items():
+            sum = v_value / population_per_vaccine
+            print(f"{key}'s Vaccine allotment:  {sum}")
+            print(f"\tPer city center, {key} will have {round(total_population_counter / value, 2)} people per vaccine")
+            total_vaccines_alloted = sum * counter_index
+
+        print(f"Total vaccines alloted for the {counter_index} countries in the {v_key} Group: {int(total_vaccines_alloted)}\n")
+
+    if v_key == "Sweden":
+        new_dict = {
+            'Population': 0
+        } 
+
+        total_population_counter = 0
+        counter_index = 0
+
+        for key, value in sweden_dict.items():
+#           print(f"{key} has {value} people")
+            total_population_counter += value
+            counter_index += 1
+            new_dict['Population'] += value
+
+        for key, value in new_dict.items():
+            new_dict['Population'] = value
+
+#       print(f"There are {total_population_counter} people in {counter_index} countries.")
+
+        mean = total_population_counter / counter_index
+
+        population_per_vaccine = total_population_counter / mean
+
+        for key, value in sweden_dict.items():
+            sum = v_value / population_per_vaccine
+            print(f"{key}'s Vaccine allotment:  {sum}")
+            print(f"\tPer city center, {key} will have {round(total_population_counter / value, 2)} people per vaccine")
+            total_vaccines_alloted = sum * counter_index
+
+        print(f"Total vaccines alloted for the {counter_index} countries in the {v_key} Group: {int(total_vaccines_alloted)}\n")
+
+    if v_key == "United States":
+        new_dict = {
+            'Population': 0
+        } 
+
+        total_population_counter = 0
+        counter_index = 0
+
+        for key, value in usa_dict.items():
+#           print(f"{key} has {value} people")
+            total_population_counter += value
+            counter_index += 1
+            new_dict['Population'] += value
+
+        for key, value in new_dict.items():
+            new_dict['Population'] = value
+
+#       print(f"There are {total_population_counter} people in {counter_index} countries.")
+
+        mean = total_population_counter / counter_index
+
+        population_per_vaccine = total_population_counter / mean
+
+        for key, value in usa_dict.items():
+            sum = v_value / population_per_vaccine
+            print(f"{key}'s Vaccine allotment:  {sum}")
+            print(f"\tPer city center, {key} will have {round(total_population_counter / value, 2)} people per vaccine")
+            total_vaccines_alloted = sum * counter_index
+
+        print(f"Total vaccines alloted for the {counter_index} countries in the {v_key} Group: {int(total_vaccines_alloted)}\n")
+
+# Columns I deem to be irrelevant to data collection
 merged_dataframe.drop(
     ['Capital',
      'Code2',
@@ -204,11 +394,9 @@ merged_dataframe.drop(
      'LocalName'
     ], axis=1, inplace=True)
 
-merged_dataframe.to_csv('datasets/all_countries.csv')
+merged_dataframe.to_csv('all_countries.csv')
 
-"""
 # Original Tables from Database
-result_dataFrame1.to_csv('datasets/DF_1.csv')
-result_dataFrame2.to_csv('datasets/DF_2.csv')
-result_dataFrame3.to_csv('datasets/DF_3.csv')
-"""
+result_dataFrame1.to_csv('DF_1.csv')
+result_dataFrame2.to_csv('DF_2.csv')
+result_dataFrame3.to_csv('DF_3.csv')
